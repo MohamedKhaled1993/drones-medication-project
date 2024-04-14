6904e498d2cddf391b60ac8abd5c10ed6128fb97
MohamedKhaled1993 <166968804+MohamedKhaled1993@users.noreply.github.com>
2024-04-14 21:04:07 +0200

    final commit

--- src/main/java/com/drones/exceptions/DroneFeetSizeExceeded.java
+++ src/main/java/com/drones/exceptions/DroneFeetSizeExceeded.java
@@ -1,7 +1,6 @@
 package com.drones.exceptions;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
